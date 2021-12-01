2ffb838f11dbdf17858d87001a309f66e9dbb073
CIGR2021 <appencigr@gmail.com>
2021-12-01 11:35:58 -0300

    peguei o VIM CODE e dividir em 3 partes.

---
+++ Criptografar.js
@@ -0,0 +1,24 @@
+const VIM_CODE = "1HGCM82633A004352"
+
+let vimCodePart2 = '';
+let vimCodePart1 = '';
+let vimCodePart3 = '';
+
+const separarVimCode = () => {
+  for (let indice = 0; indice <= VIM_CODE.length - 1; indice++) {
+    if (indice <= 3) {
+      vimCodePart1 += VIM_CODE[indice];
+    } else if (indice >3 && indice <= 9) {
+      vimCodePart2 += VIM_CODE[indice];
+    } else {
+      vimCodePart3 += VIM_CODE[indice];
+    }
+  }
+}
+
+for (let indice = 0; indice <= vimCodePart1.length; indice++) {
+  
+}
+
+
+// console.log(vimCodePart1, vimCodePart2, vimCodePart3)
